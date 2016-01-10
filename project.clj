39ad0e93e0dc3b11d9854477f41bf06e9efc3dca
Hans Huebner <hans.huebner@gmail.com>
2016-01-10 09:03:33 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.telenordigital.data-insights/clj-oauth2 "0.5.8"
+(defproject hanshuebner/clj-oauth2 "0.5.8"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :url "https://github.com/13o6/clj-oauth2"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
-                 [clj-http "1.1.0"]
+                 [clj-http "2.0.0"]
                  [uri "1.1.0"]
-                 [commons-codec/commons-codec "1.6"]
+                 [commons-codec/commons-codec "1.10"]
                  [ring "1.4.0"]]
   :aot [clj-oauth2.OAuth2Exception
         clj-oauth2.OAuth2StateMismatchException])
